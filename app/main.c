4a9b7ca3460d08577f7d543063ced6a5b757610c
rakshitraj <hello@rakshitraj.com>
2023-10-08 23:49:39 +0530

    task 2

--- app/main.c
+++ app/main.c
@@ -2,6 +2,9 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+// File descriptor
+typedef int pipefd_t[2];
+
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 int main(int argc, char *argv[]) {
 	// Disable output buffering
@@ -10,7 +13,14 @@ int main(int argc, char *argv[]) {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	printf("Logs from your program will appear here!\n");
 
-	// Uncomment this block to pass the first stage
+	// Decalare pipe file descriptors for output and error
+	pipefd_t fdOut;
+	pipefd_t fdErr;
+
+	if (pipe(fdOut) == -1 || pipe(fdErr) == -1) {
+		printf("Error initializing pipe.");
+		return 2;
+	}
 	
 	char *command = argv[3];
 	int child_pid = fork();
@@ -20,12 +30,46 @@ int main(int argc, char *argv[]) {
 	}
 	
 	if (child_pid == 0) {
-		   // Replace current program with calling program
-	    execv(command, &argv[3]);
+		// In the child process
+		// Replace current program with calling program
+		close(fdOut[0]);
+		close(fdErr[0]);
+
+		dup2(fdOut[1], STDOUT_FILENO);
+		dup2(fdErr[1], STDERR_FILENO);
+
+		execv(command, &argv[3]);
+
+		close(fdOut[1]);
+		close(fdErr[1]);
 	} else {
-		   // We're in parent
-		   wait(NULL);
-		   printf("Child terminated");
+		// We're in parent
+		close(fdOut[0]);
+		close(fdErr[0]);
+
+		int len;
+		char buffer_out[1024];
+		char buffer_err[1024];
+
+		// Read output buffer
+		if((len = read(fdOut[0], buffer_out, 1024)) == -1) {
+			printf("Error reading output...");
+			return -1;
+		}
+		write(fdOut[1], buffer_out, len);
+
+		// Read output buffer
+		if((len = read(fdErr[0], buffer_err, 1024)) == -1) {
+			printf("Error reading err...");
+			return -1;
+		}
+		write(fdErr[1], buffer_err, len);
+
+		close(fdOut[1]);
+		close(fdErr[1]);
+
+		wait(NULL);
+	    printf("Child terminated");
 	}
 
 	return 0;
